94f8b7e0e76ed282b182f429ecb212eab74e58b2
Mateus F <mateusf2108@gmail.com>
2025-04-08 02:21:09 -0300

    Alteração favicon e title

--- MiniBank/src/main.jsx
+++ MiniBank/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import theme from './styles/theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ChakraProvider theme={theme}>
     <App />
+    </ChakraProvider> 
   </StrictMode>,
 )
