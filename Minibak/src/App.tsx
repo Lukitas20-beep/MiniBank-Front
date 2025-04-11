2c56a3199a5fbfda2df14eeff65c796a60adaf82
Arthur5502 <arthurfelipecampos@hotmail.com>
2025-04-11 12:42:07 -0300

    [feat]: Alter the App

--- Minibak/src/App.tsx
+++ Minibak/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from '/MB.svg'
 import './App.css'
 
 function App() {
@@ -9,24 +9,21 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://github.com/Arthur5502/MiniBank-Front.git" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>MiniBank</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button>
+          Login
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Welcome to our banking app! Here, you can manage your finances with ease.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click on the MiniBank logo to learn more
       </p>
     </>
   )
