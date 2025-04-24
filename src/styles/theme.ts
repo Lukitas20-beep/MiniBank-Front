6dce881b3cd8dbe43493863738ad869ac0664378
Arthur_FCampos <162643192+Arthur5502@users.noreply.github.com>
2025-04-24 09:21:49 -0300

    Revert "atualização na home.tsx"

--- src/styles/theme.ts
+++
@@ -1,9 +0,0 @@
-import { extendTheme } from '@chakra-ui/react'
-
-const customTheme = {
-  // Aqui você pode colocar colors, fonts, components, etc.
-};
-
-const theme = extendTheme(customTheme);
-
-export default theme
