30a5b65dfee6f24a569eade7f227173e58c75de6
Arthur5502 <arthurfelipecampos@hotmail.com>
2025-05-22 17:09:47 -0300

    [fix]: melhorei algumas coisas

--- Minibank/src/components/Sidebar.tsx
+++ Minibank/src/components/Sidebar.tsx
@@ -68,7 +68,7 @@ const SidebarItem = ({ icon, label, path, onClick }: SidebarItemProps) => {
 
 const SidebarContent = ({ onClose }: { onClose?: () => void }) => (
     <Box
-        w={{ base: '60vw', md: '15vh' }} // Largura ajustada para 60vw no modo base (mobile) - REVERTIDO PARA O VALOR ANTERIOR
+        w={{ base: '60vw', md: '20vh' }} // Largura ajustada para 60vw no modo base (mobile) - REVERTIDO PARA O VALOR ANTERIOR
         bg="gray.50"
         py={6}
         px={4} // Aumenta o padding horizontal
